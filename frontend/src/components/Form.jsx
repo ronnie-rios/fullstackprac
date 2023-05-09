dc0853ac12becd2438d8224eda48507533586f4b
Ronnie Rios <ronniesmail14@gmail.com>
2023-05-09 15:13:39 -0500

    form added

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,7 +1,31 @@
-const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 
-export default Form
+const Form = () => {
+  const [form, setForm] = useState({
+    todo: "",
+  });
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <>
+      <h2>Create a To-Do</h2>
+      <form>
+        <input
+          type="text"
+          name="todo"
+          value={form.todo}
+          onChange={formHandler}
+          placeholder="Finish project. . ."
+        />
+      </form>
+    </>
+  );
+};
+
+export default Form;
